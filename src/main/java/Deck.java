83be70532a1b05646acd167cd5271d18d03d299e
itsariadust <jakobaindreas11@gmail.com>
2024-09-29 02:01:42 +0800

    Made the deck a 416-card deck

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -6,26 +6,34 @@ class Deck {
 	
 	public Deck() {
 		cards = new Stack<>();
+		// Arrays of the ranks, suits, and values
+		// Note: Find a way to make the ace a 1 or an 11
 		String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
         String[] suits = {"H", "D", "C", "S"};
         Integer[] values = {2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11};
-        
-        for (String suit : suits) {
-        	for (int i = 0; i < ranks.length; i++) {
-        		cards.push(new Card(ranks[i], suit, values[i]));
-        	}
-        }
-        Collections.shuffle(cards);
+
+		// Create a deck equivalent to eight 52-card decks
+		int decksCount = 8;
+		for (int deck = 0; deck < decksCount; deck++) {
+			for (String suit : suits) {
+				for (int rankIndex = 0; rankIndex < ranks.length; rankIndex++) {
+					String rank = ranks[rankIndex];
+					int value = values[rankIndex];
+					cards.push(new Card(rank, suit, value));
+				}
+			}
+		}
+
+		// Shuffle the deck
+		Collections.shuffle(cards);
 	}
-	
+
+	// Draw a card from the deck
 	public Card drawCard() {
 		return cards.isEmpty() ? null : cards.pop();
 	}
-	
-	public boolean isEmpty() {
-        return cards.isEmpty();
-    }
 
+	// Checks the remaining cards in the deck
 	public Integer checkRemainingCards() {
 		return cards.size();
 	}
